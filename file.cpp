a85a90f54561c88983996f2a1f28225a9da3695d
Vladis <you@example.com>
2022-01-02 15:05:56 +0300

    Test commit

--- file.cpp
+++ file.cpp
@@ -3,22 +3,8 @@
 
 int main()
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
 	std::cout<<"Welcome!\n";
-<<<<<<< Updated upstream
 	std::cout<<"Add ME!\n";
-=======
-	std::cout<<"Welcome!"<<"Algorithm";
-<<<<<<< HEAD
->>>>>>> master
-=======
-	std::cout<<"It will be rebased!";
-=======
->>>>>>> Stashed changes
->>>>>>> master
-=======
 	std::cout<<"Welcome to my World";
->>>>>>> master
 	system("pause");
 }
